5d4a779fc1a17c70ddab6b512bb4bd040d6b2556
Kannan Manickam <kannan@rightscale.com>
2013-03-03 12:48:23 -0800

    added setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Setting up vim..."
+
+timestamp=$(date +"%Y%m%d%H%M")
+if [ -d ~/.vim ];
+then
+    echo "Taking a backup of existing vim setup.."
+    mv ~/.vim ~/.vim.$timestamp
+fi
+git clone git://github.com/arangamani/vimrc ~/.vim
+cd ~/.vim
+git submodule init
+git submodule update
+
+if [ -f ~/.vimrc ];
+then
+    echo "Taking a backup of existing vimrc..."
+    mv ~/.vimrc ~/.vimrc.$timestamp
+fi
+ln -s ~/.vim/vimrc ~/.vimrc
+
+echo "Completed setup of vim..."
