653fc39ed19165680fd921afdc5fae8a37bfb429
Mike Metral <metral@gmail.com>
2015-08-05 15:01:05 -0700

    Add lua support for neocomplete

--- setup.sh
+++ setup.sh
@@ -3,7 +3,36 @@
 echo "Setting up vim..."
 
 sudo apt-get update
-sudo apt-get install -y git build-essential vim cmake python-dev python3-dev < "/dev/null"
+sudo apt-get install -y git build-essential vim-nox cmake python-dev python3-dev < "/dev/null"
+
+# Install lua from binaries
+sudo apt-get install liblua5.1-dev luajit libluajit-5.1 -y
+
+# Remove old vims
+sudo apt-get remove vim vim-runtime gvim -y
+sudo apt-get remove vim-tiny vim-common vim-gui-common -y
+sudo rm -rf /usr/local/bin/vim /usr/bin/vim
+
+sudo mkdir /usr/include/lua5.1/include
+sudo mv /usr/include/lua5.1/*.h /usr/include/lua5.1/include/
+
+hg clone https://vim.googlecode.com/hg/ ~/vim
+pushd ~/vim
+hg pull
+hg update
+pushd src
+sudo make distclean
+./configure --with-features=huge \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7-config \
+            --enable-perlinterp \
+            --enable-cscope --prefix=/usr \
+            --enable-luainterp \
+            --with-luajit \
+            --with-lua-prefix=/usr/include/lua5.1
+sudo make -j4 VIMRUNTIMEDIR=/usr/share/vim/vim74
+sudo make install
 
 git clone git://github.com/metral/vimrc ~/.vim
 pushd ~/.vim
@@ -13,10 +42,10 @@ git submodule update
 
 ln -s ~/.vim/vimrc ~/.vimrc
 
-pushd bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.sh --clang-completer --gocode-completer
-popd
+#pushd bundle/YouCompleteMe
+#git submodule update --init --recursive
+#./install.sh --clang-completer --gocode-completer
+#popd
 
 vim +PluginInstall +GoInstallBinaries +qall > /dev/null 2>&1
 popd
