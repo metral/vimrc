2ac0854dfd1e4b3c1b50a7ab3a30a5e671a0e614
Mike Metral <1112768+metral@users.noreply.github.com>
2022-07-16 12:26:26 -0700

    Cleanup unused gocode

--- update.sh
+++ update.sh
@@ -9,11 +9,4 @@ git add .
 git commit -m "Submodule sync"
 git push
 
-#vim +PluginInstall! +GoInstallBinaries! +qall now > /dev/null 2>&1
-#vim -E -u NONE -S ~/.vimrc +PluginInstall +GoInstallBinaries +qall > /dev/null
 vim -E -u NONE -S ~/.vimrc +PluginInstall +GoInstallBinaries +GoUpdateBinaries +qall
-rm -rf $GOPATH/bin/gomod
-ps aux | grep gocode-gomod | awk '{print $2}' | xargs kill -9
-ps aux | grep gocode | awk '{print $2}' | xargs kill -9
-go get -u github.com/stamblerre/gocode
-gocode close
